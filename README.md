caa726fd57ff640b9b6c25cc725ad3b9e9889934
Rory King <rory.king@dazn.com>
2019-08-07 14:14:17 +0100

    Tidy up

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-apollo-react-timing-repro
+# apollo-react-timing-repro
+
+## Reproduction steps
+ - Install packages
+ - `yarn test`
+
+## Expected outcome
+Error is persisted, and both tests pass
+
+## Actual outcome
+A subsequent rerender causes the error prop to be removed - with one test consistently failing, and the other intermittently failing.
--- src/timing-bug.jsx
+++ src/timing-bug.jsx
@@ -10,20 +10,18 @@ export const TEST_QUERY = gql`
   }
 `;
 
-
-const TestComponent = ({ match }) => {
-  const [arbitaryState, setArbitaryState] = useState(null);
-  const userId = match.params.userId;
-
+const TestComponent = ({ userId }) => {
   return (
     <Query query={TEST_QUERY} variables={{ userId }}>
       {({ loading, data, error }) => {
-          console.log('Rerender of Query occured: ', { loading, data, error });
-        return (<div>
+        console.log('Rerender of Query occured: ', { loading, data, error });
+        return (
+          <div>
             {loading && <div id="loading">Loading</div>}
             {error && <div id="error">error: {JSON.stringify(error)}</div>}
             <div id="data">data: {JSON.stringify(data)}</div>
-        </div>);
+          </div>
+        );
       }}
     </Query>
   );
